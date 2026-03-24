102abf228d2748e1719f2179fd42de32c292c45f
Enrique <31453489+riqedev@users.noreply.github.com>
2026-03-24 00:16:17 -0300

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="mt-16  py-8 text-center text-zinc-600 dark:text-zinc-400 ">
-      <small className=" block text-xs">&copy; 2023 by riqedev.</small>
-    </footer>
-  );
-}
--- components/layouts/paddingContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { OnlyChildrenProps } from "@/types/generalTypes";
-
-const PaddingContainer = ({ children }: OnlyChildrenProps) => {
-  return <div className="mx-auto w-full max-w-5xl px-4 ">{children}</div>;
-};
-
-export default PaddingContainer;
